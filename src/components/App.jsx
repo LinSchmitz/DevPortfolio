56910915fe9861a2cb30a2b8682c9f4908b69505
naleilan <leilan.n@gmail.com>
2025-06-27 16:53:45 +0330

    added dotted background

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,5 +2,19 @@
 import React from 'react';
 
 export default function App() {
-  return <div>DevPortfolio</div>;
+  return (
+    <div className="container">
+      <h1>Navbar</h1>
+      <div className="sec">
+        <section className="left">left</section>
+        <section className="right">right</section>
+      </div>
+      <div className="socials">
+        <button className="btn btn-f">f</button>
+        <button className="btn btn-x">X</button>
+        <button className="btn btn-in">in</button>
+        <button className="btn btn-g">g</button>
+      </div>
+    </div>
+  );
 }
